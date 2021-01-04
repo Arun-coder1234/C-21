cb645e10c48947c65b328d41e90c27765398cbc0
Arun-coder1234 <74406147+Arun-coder1234@users.noreply.github.com>
2021-01-04 15:36:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var fixedRect, movingRect;
+var gameObject1, gameObject2, gameObject3, gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+ //fixedRect =  createSprite(600, 400, 50, 80);
+ fixedRect =  createSprite(200, 100, 50, 80);
+ fixedRect.shapeColor = "blue";
+ fixedRect.velocityX = 5;
+
+
+ //movingRect =  createSprite(400, 200, 80, 30);
+ movingRect =  createSprite(400, 100, 80, 30);
+ movingRect.shapeColor = "blue";
+ movingRect.velocityX = -5;
+
+ gameObject1 = createSprite(100, 100, 50, 50);
+ gameObject1.shapeColor = "green";
+
+ gameObject2 = createSprite(200, 100, 50, 50);
+ gameObject2.shapeColor = "green";
+
+ gameObject3 = createSprite(300, 100, 50, 50);
+ gameObject3.shapeColor = "green";
+
+ gameObject4 = createSprite(400, 100, 50, 50);
+ gameObject4.shapeColor = "green";
+}
+
+
+
+function draw() {
+  background(0, 0, 0);  
+
+  /*movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+
+ if(isTouching(movingRect, gameObject2)){
+  gameObject2.shapeColor = "red";
+  movingRect.shapeColor = "red";
+ }else{
+  gameObject2.shapeColor = "yellow";
+  movingRect.shapeColor = "yellow";
+ }*/
+
+ bounceOff(movingRect,fixedRect);
+
+
+
+  drawSprites();
+}
+
